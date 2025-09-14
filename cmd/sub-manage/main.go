2ce1c5059f4b4d722858d207828549546c39f855
bobro-a <m3_ba@mail.ru>
2025-09-14 16:42:41 +0300

    made main file

---
+++ cmd/sub-manage/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+	"sub-manage/internal/app"
+	"sub-manage/pkg/config"
+)
+
+func main() {
+	log.Println("Start sub-manage service")
+
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatalf("Failed ti load config: %v", err)
+	}
+	log.Printf("Loaded config: DB=%s, Migrations=%s",
+		cfg.Database.URL,
+		cfg.Migrations.Path)
+
+	ctx := context.Background()
+
+	a, err := app.New(ctx, cfg)
+	if err != nil {
+		log.Fatalf("Failed to init app: %v", err)
+	}
+	log.Println("App initialized success! Starting service...")
+
+	if err = a.Start(); err != nil {
+		log.Fatalf("Failed to start app: %v", err)
+	}
+}
