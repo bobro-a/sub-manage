01364ad8287c638687a402fedd9c5afd0d3201d4
bobro-a <m3_ba@mail.ru>
2025-09-14 16:16:51 +0300

    made struct app for run database and server

---
+++ internal/app/app.go
@@ -0,0 +1,50 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"sub-manage/pkg/config"
+
+	"github.com/golang-migrate/migrate/v4"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+type App struct {
+	ctx context.Context
+	cfg *config.Config
+	db  *sqlx.DB
+}
+
+func New(ctx context.Context, cfg *config.Config) (*App, error) {
+	db, err := sqlx.Connect("postgres", cfg.Database.URL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to DB: %w", err)
+	}
+	if err = db.Ping(); err != nil {
+		return nil, fmt.Errorf("database is unreachable: %w", err)
+	}
+	return &App{
+		ctx: ctx,
+		cfg: cfg,
+		db:  db,
+	}, nil
+}
+
+func (a *App) Start() error {
+	defer func() {
+		_ = a.db.Close()
+	}()
+
+	m, err := migrate.New(a.cfg.Migrations.Path, a.cfg.Database.URL)
+	if err != nil {
+		return fmt.Errorf("failed to init migration: %w", err)
+	}
+	if err = m.Up(); err != nil && err.Error() != "no change" {
+		return fmt.Errorf("failed to run migretion: %w", err)
+	}
+	//todo start server for http request
+
+	return nil
+}
